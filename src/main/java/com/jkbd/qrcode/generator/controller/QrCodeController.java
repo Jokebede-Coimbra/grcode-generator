6138bd7117ac7be4858aba2ebb970f86137bdc50
Coimbra <adsjkbd@gmail.com>
2025-05-02 21:33:19 -0300

    Criando projeto,

---
+++ src/main/java/com/jkbd/qrcode/generator/controller/QrCodeController.java
@@ -0,0 +1,9 @@
+package com.jkbd.qrcode.generator.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("qrcode")
+public class QrCodeController {
+}
