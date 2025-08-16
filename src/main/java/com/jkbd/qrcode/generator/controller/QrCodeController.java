b62a8669fc9dbbec23ca3d0f44026c2f60371223
Coimbra <adsjkbd@gmail.com>
2025-08-16 16:35:52 -0300

    feat: Dockerização e variáveis de ambiente

--- src/main/java/com/jkbd/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/jkbd/qrcode/generator/controller/QrCodeController.java
@@ -5,6 +5,7 @@ import com.jkbd.qrcode.generator.dto.QrCodeGenerateRequest;
 import com.jkbd.qrcode.generator.dto.QrCodeGenerateResponse;
 import com.jkbd.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class QrCodeController {
         this.qrCodeGeneratorService = qrCodeGeneratorService;
     }
 
+    @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) throws IOException, WriterException {
         try {
             QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
